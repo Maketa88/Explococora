18d9c27c70e5219f65886cb9c907ce77748679bc
Brayan Stiven Gutierrez Higuita <brayanstevengutierrezhiguita@gmail.com>
2024-12-16 12:02:28 -0500

    modificando menu hamburguesa

--- src/components/PaginaInicio/Header/Logo.jsx
+++ src/components/PaginaInicio/Header/Logo.jsx
@@ -3,12 +3,8 @@ import Explo from "../../../assets/Images/ejemplo.jpg";
 
 export const Logo = () => {
   return (
-    <div className="flex items-center">
-      <img
-        src={Explo}
-        alt="Logo de Explococora"
-        className="h-10 w-auto"
-      />
+    <div className="relative z-0">
+      <img src={Explo} alt="Logo de Explococora" className="h-10 w-auto" />
     </div>
   );
 };
