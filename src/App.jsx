b7ad16a684f5dc2fd82afbc5242e5e761f656fcb
Brayan Stiven Gutierrez Higuita <brayanstevengutierrezhiguita@gmail.com>
2024-12-16 12:00:53 -0500

    agregando componente pagina principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import { Header } from "./components/PaginaInicio/Header/Header";
+import { PaginaInicio } from "./pages/PaginaInicio/PaginaInicio";
+PaginaInicio;
 
 function App() {
   return (
     <>
-      <Header />
+      <PaginaInicio />
     </>
   );
 }
