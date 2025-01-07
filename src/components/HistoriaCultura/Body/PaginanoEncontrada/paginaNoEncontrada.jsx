173822b5762e1107616c82be2300b2df312a7de7
Brayan Stiven Gutierrez Higuita <brayanstevengutierrezhiguita@gmail.com>
2025-01-07 10:54:17 -0500

    degradado gris

--- src/components/HistoriaCultura/Body/PaginanoEncontrada/paginaNoEncontrada.jsx
+++ src/components/HistoriaCultura/Body/PaginanoEncontrada/paginaNoEncontrada.jsx
@@ -3,7 +3,7 @@ import error from "../../../../assets/Images/paisaje.svg";
 
 export const PaginaNoEncontrada = () => {
   return (
-    <div className="flex justify-center items-center min-h-full ">
+    <div className="flex justify-center items-center min-h-full bg-gradient-to-r from-gray-300 via-gray-200 to-gray-100">
       <div className=" max-w-3xl">
         <img
           src={error} // Reemplaza con tu imagen
