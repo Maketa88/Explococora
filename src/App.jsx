b67f152060cdd83bf15df50e139cec847b1c0cee
Gabriel Rubio <gabrielrubiosuarez@gmail.com>
2025-03-19 18:40:49 -0500

    Pantalla Carga

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-// Explococora/src/App.jsx
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AuthProvider } from "./context/AuthContext";
 import { RutasExplococora } from "./routes/RutasExplococora/RutasExplococora";
+import LoadingScreen from "./components/LoadingScreen/LoadingScreen";
+import { motion, AnimatePresence } from "framer-motion";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
   return (
-    <>
-      <AuthProvider>
-        <RutasExplococora />
-      </AuthProvider>
-    </>
+    <AuthProvider>
+      <LoadingScreen isLoading={loading} />
+      <AnimatePresence>
+        {!loading && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <RutasExplococora />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </AuthProvider>
   );
 };
 
